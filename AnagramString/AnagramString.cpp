92cb3fadc8a332d5a52eb5afce36675238bcda08
rushikeshnakhate@gmail.com <rushikeshnakhate@gmail.com>
2022-09-03 13:29:34 +0800

    anagram of string

---
+++ AnagramString/AnagramString.cpp
@@ -0,0 +1,14 @@
+//Please wahtch https://www.youtube.com/watch?v=XFh_AoEdOTw
+
+#include <vector>
+#include <string>
+
+int main() {
+    std::string s1 = "XYYZXZYZXXYZ";
+    std::string s2 = "XYZ";
+
+    std::vector<int> s1Hash(26, 0);
+    std::vector<int> s2Hash(26, 0);
+
+    return 0;
+}
